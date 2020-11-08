9ff7fcbda921b376245cd9c28d4f4a8f1508425e
Gabriel Bellon <gabriel.bellon@contaazul.com>
2020-11-08 19:15:37 -0300

    refactor

--- streamyard_download.py
+++ streamyard_download.py
@@ -11,159 +11,161 @@ from loguru import logger
 
 import config
 
-
-def download_file(file_name, request_session, request_url):
-    show_log = True
-    previus_done = None
-    with open(os.path.join(config.LOCAL_DOWNLOAD_PATH, file_name), "wb") as f:
-        print(
-            f"Downloading {file_name} para {os.path.join(config.LOCAL_DOWNLOAD_PATH,file_name)}"
-        )
-
-        response = request_session.get(request_url, stream=True)
-        total_length = response.headers.get("content-length")
-
-        if total_length is None:
-            f.write(response.content)
-        else:
-            dl = 0
-            total_length = int(total_length)
-            for data in response.iter_content(chunk_size=4096):
-                dl += len(data)
-                f.write(data)
-                done = int(100 * dl / total_length)
-
-                if done != previus_done:
-                    show_log = True
-                if done in range(0, 100, 5) and show_log:
-                    logger.info(f"Donwload do arquivo {file_name} em {done}%")
-                    show_log = False
-                    previus_done = done
-
-def create_session():
-    logger.info("Criando Sessão")
-    return requests.session()
-
-
-def get_cookie(request_session):
-    logger.info("Carregando Cookie")
-    request_session.get(config.TOKEN_URL)
-    config.TOKEN = request_session.cookies["csrfToken"]
-
-
-def payload_token():
-    return dict(email=config.EMAIL, csrfToken=config.TOKEN)
-
-
-def request_code(request_session):
-    logger.info("Requisitando Código")
-    request_session.post(
-        config.CODE_URL, data=payload_token(), headers=dict(Referer=config.CODE_URL)
-    )
-
-
-def read_email_code():
-    logger.info("Carregando Cookie")
-    return input(f"Insira o código de login enviado para {config.EMAIL}:")
-
-
-def payload_login(email_code):
-    return dict(email=config.EMAIL, csrfToken=config.TOKEN, otpToken=email_code)
-
-
-def login_streamyard(request_session):
-    logger.info("Acessando StreamYard")
-    request_session.post(
-        config.LOGIN_URL,
-        data=payload_login(read_email_code()),
-        headers=dict(Referer=config.LOGIN_URL),
-    )
-
-
-def list_past_broadcast(request_session):
-    logger.info("Carregando Broadcast")
-    last_broadcast = request_session.get(config.LIST_PAST_URL)
-    return json.loads(last_broadcast.text).get("broadcasts")
-
-
-def create_download_list(request_session):
-    broadcasts = list_past_broadcast(request_session)
-    logger.info("Separando Broadcast Para Download")
-    broadcast_to_download = []
-    for broadcast in broadcasts:
-        file_name = re.sub("[\W]+", "", broadcast.get("title").replace(" ", "_"))
-        broadcast_to_download.append(
-            dict(
-                stream_id=broadcast.get("id"),
-                file_name=file_name,
-                audio_filename=f"{file_name}.mp3",
-                video_filename=f"{file_name}.mp4",
-            )
-        )
-    return broadcast_to_download
-
-
-def start_donwload(request_session):
-    broadcast_to_download = create_download_list(request_session)
-
-    with ProcessPoolExecutor(max_workers=int(config.MAX_THREADS)) as executor:
-        future_executor = {
-            executor.submit(download_broadcast, request_session, item): item.get(
-                "stream_id"
-            )
-            for item in broadcast_to_download
-        }
-
-    for future in futures.as_completed(future_executor):
-        file_name = future.result()
-        logger.info(f"Download da stream {file_name} completo ")
-
-
-def download_broadcast(request_session, stream_info):
-    stream_id = stream_info.get("stream_id")
-    file_name = stream_info.get("file_name")
-    video_filename = stream_info.get("video_filename")
-    audio_filename = stream_info.get("audio_filename")
-
-    logger.info(f"Download stream id:{stream_id} name:{file_name}")
-
-    while True:
-        logger.info(f"Gerando Links de donwload")
-
-        make_urls = request_session.get(
-            config.CREATE_DOWNLOADS_URL.format(stream_id=stream_id)
-        )
-        status = json.loads(make_urls.text).get("status")
-
-        logger.info(f"Status: {status}")
-
-        if status != "creating":
-            break
-
-        time.sleep(10)
-
-    download_url = request_session.get(config.DOWNLOAD_URL.format(stream_id=stream_id))
-
-    logger.info(f"Download do audio")
-    download_file(
-        file_name=audio_filename,
-        request_session=request_session,
-        request_url=json.loads(download_url.text).get("audioUrl"),
-    )
+class StreamYardDownload():
     
-    logger.info(f"Download do video")
-    download_file(
-        file_name=video_filename,
-        request_session=request_session,
-        request_url=json.loads(download_url.text).get("videoUrl"),
-    )
+    def __init__(self):
+        self.request_session = self.create_session()
 
-    return file_name
+    def create_session(self):
+        logger.info("Criando Sessão")
+        return requests.session()
+        
+    def download_file(self,file_name,request_url):
+        show_log = True
+        previus_done = None
+        with open(os.path.join(config.LOCAL_DOWNLOAD_PATH, file_name), "wb") as f:
+            print(
+                f"Downloading {file_name} para {os.path.join(config.LOCAL_DOWNLOAD_PATH,file_name)}"
+            )
 
+            response = self.request_session.get(request_url, stream=True)
+            total_length = response.headers.get("content-length")
+
+            if total_length is None:
+                f.write(response.content)
+            else:
+                dl = 0
+                total_length = int(total_length)
+                for data in response.iter_content(chunk_size=4096):
+                    dl += len(data)
+                    f.write(data)
+                    done = int(100 * dl / total_length)
+
+                    if done != previus_done:
+                        show_log = True
+                    if done in range(0, 100, 5) and show_log:
+                        logger.info(f"Donwload do arquivo {file_name} em {done}%")
+                        show_log = False
+                        previus_done = done
+
+    def get_cookie(self):
+        logger.info("Carregando Cookie")
+        self.request_session.get(config.TOKEN_URL)
+        self.TOKEN = self.request_session.cookies["csrfToken"]
+
+    @staticmethod
+    def payload_token(token):
+        return dict(email=config.EMAIL, csrfToken=token)
+
+    def request_code(self):
+        logger.info("Requisitando Código")
+
+        self.request_session.post(
+            config.CODE_URL, 
+            data=self.payload_token(self.TOKEN), 
+            headers=dict(Referer=config.CODE_URL)
+        )
+
+    @staticmethod
+    def read_email_code():
+        logger.info("Carregando Cookie")
+        return input(f"Insira o código de login enviado para {config.EMAIL}:")
+
+    @staticmethod 
+    def payload_login(token,email_code):
+        return dict(email=config.EMAIL, csrfToken=token, otpToken=email_code)
+
+    def login(self):
+        logger.info("Acessando StreamYard")
+
+        self.get_cookie()
+        self.request_code()
+
+        self.request_session.post(
+            config.LOGIN_URL,
+            data=self.payload_login(self.TOKEN,self.read_email_code()),
+            headers=dict(Referer=config.LOGIN_URL),
+        )
+
+    def list_past_broadcast(self):
+        logger.info("Carregando Broadcast")
+        last_broadcast = self.request_session.get(config.LIST_PAST_URL)
+        return json.loads(last_broadcast.text).get("broadcasts")
+
+    def create_download_list(self):
+        broadcasts = self.list_past_broadcast()
+        logger.info("Separando Broadcast Para Download")
+        broadcast_to_download = []
+        for broadcast in broadcasts:
+            file_name = re.sub("[\W]+", "", broadcast.get("title").replace(" ", "_"))
+            broadcast_to_download.append(
+                dict(
+                    stream_id=broadcast.get("id"),
+                    file_name=file_name,
+                    audio_filename=f"{file_name}.mp3",
+                    video_filename=f"{file_name}.mp4",
+                )
+            )
+        return broadcast_to_download
+
+    def dowload(self):
+        broadcast_to_download = self.create_download_list()
+
+        with ProcessPoolExecutor(max_workers=int(config.MAX_THREADS)) as executor:
+            future_executor = {
+                executor.submit(self.download_broadcast, self.request_session, item): item.get(
+                    "stream_id"
+                )
+                for item in broadcast_to_download
+            }
+
+        for future in futures.as_completed(future_executor):
+            file_name = future.result()
+            logger.info(f"Download da stream {file_name} completo ")
+
+    def download_broadcast(self,stream_info):
+        stream_id = stream_info.get("stream_id")
+        file_name = stream_info.get("file_name")
+        video_filename = stream_info.get("video_filename")
+        audio_filename = stream_info.get("audio_filename")
+
+        logger.info(f"Download stream id:{stream_id} name:{file_name}")
+
+        while True:
+            logger.info(f"Gerando Links de donwload")
+
+            make_urls = self.request_session.get(
+                config.CREATE_DOWNLOADS_URL.format(stream_id=stream_id)
+            )
+            status = json.loads(make_urls.text).get("status")
+
+            logger.info(f"Status: {status}")
+
+            if status != "creating":
+                break
+
+            time.sleep(10)
+
+        download_url = self.request_session.get(config.DOWNLOAD_URL.format(stream_id=stream_id))
+
+        logger.info(f"Download do audio")
+        self.download_file(
+            file_name=audio_filename,
+            request_url=json.loads(download_url.text).get("audioUrl"),
+        )
+
+        logger.info(f"Download do video")
+        self.download_file(
+            file_name=video_filename,
+            request_url=json.loads(download_url.text).get("videoUrl"),
+        )
+
+        return file_name
+
+    def start_download(self):
+       self.login()
+       self.dowload()
 
 if __name__ == "__main__":
-    request_session = create_session()
-    get_cookie(request_session)
-    request_code(request_session)
-    login_streamyard(request_session)
-    start_donwload(request_session)
+    streamyardown = StreamYardDownload()
+    streamyardown.start_download()
